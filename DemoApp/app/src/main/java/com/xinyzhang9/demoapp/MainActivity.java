ad0fff997d05982a76bd72ea5192e29a61953e35
Xinyang <xinyzhang9@gmail.com>
2017-04-07 16:08:38 -0700

    button

--- DemoApp/app/src/main/java/com/xinyzhang9/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/xinyzhang9/demoapp/MainActivity.java
@@ -2,8 +2,14 @@ package com.xinyzhang9.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        Log.i("Info","Button pressed!");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
